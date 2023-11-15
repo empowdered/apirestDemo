cb952ef975c52fed1666c2c4838a4134092b997a
juan pablo munoz <juanp.mleiva@gmail.com>
2023-11-15 10:54:22 -0300

    first commit

---
+++ src/main/java/com/example/apirest/entities/Persona.java
@@ -0,0 +1,42 @@
+package com.example.apirest.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="persona")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Persona implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_persona")
+    private long idPersona;
+
+    @Column(name="nombre_persona")
+    private String nombrePersona;
+
+    @Column(name="apellido_persona")
+    private String apellidoPersona;
+
+    @Column(name="rut_persona")
+    private String rutPersona;
+
+    @Column(name="fecha_creacion", nullable = true)
+    private Date fechaCreacion;
+
+}
---
+++ src/main/java/com/example/apirest/services/PersonaService.java
@@ -0,0 +1,33 @@
+package com.example.apirest.services;
+
+import com.example.apirest.entities.Persona;
+
+import java.util.List;
+
+public class PersonaService implements BaseService<Persona>{
+
+    @Override
+    public List<Persona> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona save(Persona entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona update(Long id, Persona entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
